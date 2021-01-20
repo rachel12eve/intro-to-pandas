65dd4063e946250d2e39294baa74fc89f1d9212e
rachel12eve <74518402+rachel12eve@users.noreply.github.com>
2021-01-20 12:46:50 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -9,8 +9,18 @@
 #
 #     print(temperatures)
 
-import pandas
+import pandas as pd
 
-data=pandas.read_csv("weather_data.csv")
-print(data["temp"])
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
+fur = data["Primary Fur Color"]
+# print(fur)
+
+grey = len(fur[fur == "Gray"])
+red = len(fur[fur == "Cinnamon"])
+black = len(fur[fur == "Black"])
+fur_summary = {"Color": ["Gray", "Cinnamon", "Black"],
+               "Count": [grey, red, black]
+               }
+
+pd.DataFrame(fur_summary).to_csv("Fur Color Count")
